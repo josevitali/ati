7401c030b64e154493bc045cd3b285da69b18444
Francisco Bartolomé <fbartolome18@gmail.com>
2018-03-31 18:58:19 -0300

    show app icon

--- tp/src/main/java/ar/edu/itba/ati/App.java
+++ tp/src/main/java/ar/edu/itba/ati/App.java
@@ -9,11 +9,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Method;
+import java.net.URL;
 
 public class App extends Application{
 
-    //TODO sacar cabezeada
     public static Stage primaryStage;
 
     public static void main( String[] args ){
@@ -31,6 +36,18 @@ public class App extends Application{
     public void start(final Stage primaryStage) throws Exception {
         final Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/app.fxml"));
         primaryStage.setTitle("ATI Image Editor");
+        primaryStage.getIcons().add(new Image("icon/icon.jpg"));
+
+        Class util = Class.forName("com.apple.eawt.Application");
+        Method getApplication = util.getMethod("getApplication", new Class[0]);
+        Object application = getApplication.invoke(util);
+        Class params[] = new Class[1];
+        params[0] = java.awt.Image.class;
+        Method setDockIconImage = util.getMethod("setDockIconImage", params);
+        URL url = App.class.getClassLoader().getResource("icon/icon.jpg");
+        java.awt.Image image = Toolkit.getDefaultToolkit().getImage(url);
+        setDockIconImage.invoke(application, image);
+        
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         this.primaryStage = primaryStage;

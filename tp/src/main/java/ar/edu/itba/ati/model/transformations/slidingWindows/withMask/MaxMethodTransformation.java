0f10be4fd90412b123a7312aca473477e75e9060
Francisco Bartolomé <fbartolome18@gmail.com>
2018-04-12 11:22:02 -0300

    fix MaxMethodTransformation

--- tp/src/main/java/ar/edu/itba/ati/model/transformations/slidingWindows/withMask/MaxMethodTransformation.java
+++ tp/src/main/java/ar/edu/itba/ati/model/transformations/slidingWindows/withMask/MaxMethodTransformation.java
@@ -5,16 +5,20 @@ import ar.edu.itba.ati.model.pictures.Picture;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class MaxMethodTransformation extends SlidingMethodWithMultiMask {
 
     private static final BiFunction<Double,Double,Double> max = (aDouble, aDouble2) -> Math.max(aDouble, aDouble2);
+    private static final Function<Double,Double> abs = px -> Math.abs(px);
 
     public MaxMethodTransformation(List<Mask> masks) {
         super(masks, new BiConsumer<Picture, List<Picture>>() {
             @Override
             public void accept(Picture picture, List<Picture> pictures) {
+                picture.mapPixelByPixel(abs);
                 for (Picture p : pictures){
+                    p.mapPixelByPixel(abs);
                     picture.mapPixelByPixel(max, p);
                 }
             }

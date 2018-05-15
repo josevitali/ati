fe0fafa301765dcb4b1860c3bcc1fb88b243a452
Francisco Bartolomé <fbartolome18@gmail.com>
2018-05-15 17:19:52 -0300

    add second cycle to pixel exchange

--- tp/src/main/java/ar/edu/itba/ati/model/transformations/borderDetection/PixelExchangeMethod.java
+++ tp/src/main/java/ar/edu/itba/ati/model/transformations/borderDetection/PixelExchangeMethod.java
@@ -4,6 +4,8 @@ import ar.edu.itba.ati.model.pictures.ColorPicture;
 import ar.edu.itba.ati.model.pictures.GreyPicture;
 import ar.edu.itba.ati.model.pictures.Picture;
 import ar.edu.itba.ati.model.transformations.PictureTransformer;
+import ar.edu.itba.ati.model.transformations.slidingWindows.withMask.Mask;
+import ar.edu.itba.ati.model.transformations.slidingWindows.withMask.Masks;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -185,6 +187,149 @@ public class PixelExchangeMethod implements PictureTransformer {
             toRemoveLout.clear();
         }
 
+
+
+
+        // Second cycle
+        Mask gaussian = Masks.gaussianMask(5,1);
+        i = 0;
+
+        while(i < 5){
+            i++;
+
+            Set<Point> toRemoveLin = new HashSet();
+            Set<Point> toAddLin = new HashSet();
+            Set<Point> toRemoveLout = new HashSet();
+            Set<Point> toAddLout = new HashSet();
+
+            // #2
+            for(Point point : lout){
+
+                if(point.x > 2 && point.y >= 2 && point.x <= colorPicture.getHeight() - 3 && point.y <= colorPicture.getWidth() - 3 &&
+                gPhiConvolution(point.x, point.y, phi, gaussian) < 0){
+                    toRemoveLout.add(point);
+                    toAddLin.add(point);
+
+                    // up
+                    if(point.x > 0 && phi[point.x - 1][point.y] == OUT){
+                        toAddLout.add(new Point(point.x - 1,point.y));
+                    }
+
+                    // down
+                    if(point.x < phi.length - 1 && phi[point.x + 1][point.y] == OUT){
+                        toAddLout.add(new Point(point.x + 1,point.y));
+                    }
+
+                    // left
+                    if(point.y > 0 && phi[point.x][point.y - 1] == OUT){
+                        toAddLout.add(new Point(point.x,point.y - 1));
+                    }
+
+                    // right
+                    if(point.y < phi[0].length - 1 && phi[point.x][point.y + 1] == OUT){
+                        toAddLout.add(new Point(point.x,point.y + 1));
+                    }
+                }
+            }
+
+            for(Point point : toRemoveLout){
+                lout.remove(point);
+                phi[point.x][point.y] = LIN;
+            }
+            for(Point point : toAddLout){
+                lout.add(point);
+                phi[point.x][point.y] = LOUT;
+            }
+            for(Point point : toAddLin){
+                lin.add(point);
+                phi[point.x][point.y] = LIN;
+            }
+
+            // #3
+            for(Point point : lin){
+                int row = point.x;
+                int col = point.y;
+
+                if(row > 0 && row < phi.length - 1 && col > 0 && col < phi[0].length - 1 &&
+                        phi[row - 1][col] != LOUT && phi[row + 1][col] != LOUT && phi[row][col - 1] != LOUT &&
+                        phi[row][col + 1] != LOUT){
+                    phi[row][col] = IN;
+                    toRemoveLin.add(point);
+                }
+            }
+
+            lin.removeAll(toRemoveLin);
+
+            toAddLin.clear();
+            toAddLout.clear();
+            toRemoveLin.clear();
+            toRemoveLout.clear();
+
+
+            //#4
+            for(Point point : lin){
+
+                if(point.x > 2 && point.y >= 2 && point.x <= colorPicture.getHeight() - 3 && point.y <= colorPicture.getWidth() - 3 &&
+                        gPhiConvolution(point.x, point.y, phi, gaussian) > 0){
+                    toRemoveLin.add(point);
+                    toAddLout.add(point);
+
+                    // up
+                    if(point.x > 0 && phi[point.x - 1][point.y] == IN){
+                        toAddLin.add(new Point(point.x - 1,point.y));
+                    }
+
+                    // down
+                    if(point.x < phi.length - 1 && phi[point.x + 1][point.y] == IN){
+                        toAddLin.add(new Point(point.x + 1,point.y));
+                    }
+
+                    // left
+                    if(point.y > 0 && phi[point.x][point.y - 1] == IN){
+                        toAddLin.add(new Point(point.x,point.y - 1));
+                    }
+
+                    // right
+                    if(point.y < phi[0].length - 1 && phi[point.x][point.y + 1] == IN){
+                        toAddLin.add(new Point(point.x,point.y + 1));
+                    }
+                }
+            }
+
+            for(Point point : toRemoveLin){
+                lin.remove(point);
+                phi[point.x][point.y] = LOUT;
+            }
+            for(Point point : toAddLout){
+                lout.add(point);
+                phi[point.x][point.y] = LOUT;
+            }
+            for(Point point : toAddLin){
+                lin.add(point);
+                phi[point.x][point.y] = LIN;
+            }
+
+            // #5
+            for(Point point : lout){
+                int row = point.x;
+                int col = point.y;
+
+                if(row > 0 && row < phi.length - 1 && col > 0 && col < phi[0].length - 1 &&
+                        phi[row - 1][col] != LIN && phi[row + 1][col] != LIN && phi[row][col - 1] != LIN &&
+                        phi[row][col + 1] != LIN){
+                    phi[row][col] = OUT;
+                    toRemoveLout.add(point);
+                }
+            }
+
+            lout.removeAll(toRemoveLout);
+
+            toAddLin.clear();
+            toAddLout.clear();
+            toRemoveLin.clear();
+            toRemoveLout.clear();
+        }
+
         for(Point out : lout){
             colorPicture.putPixel(new Double[]{255.0,255.0,0.0}, out.x, out.y);
         }
@@ -238,6 +383,19 @@ public class PixelExchangeMethod implements PictureTransformer {
         double result = (nom/den);
 
         return result < restriction ? 1 : -1;
+    }
+
+    private double gPhiConvolution(int phiRow, int phiCol, int[][] phi, Mask gaussian){
+        double ret = 0;
+        Number[][] matrix = gaussian.getMatrix();
+
+        for(int row = 0; row < matrix.length; row++){
+            for(int col = 0; col < matrix[0].length; col++){
+                ret += phi[row + phiRow - 2][col + phiCol - 2] * (Double) matrix[row][col];
+            }
+        }
+
+        return ret;
     }
 
     private void addVectors(Double[] vec1, Double[] vec2){
--- tp/src/main/java/ar/edu/itba/ati/model/transformations/smoothing/GaussianSmoothing.java
+++ tp/src/main/java/ar/edu/itba/ati/model/transformations/smoothing/GaussianSmoothing.java
@@ -1,11 +1,12 @@
 package ar.edu.itba.ati.model.transformations.smoothing;
 
 import ar.edu.itba.ati.model.transformations.slidingWindows.withMask.Mask;
+import ar.edu.itba.ati.model.transformations.slidingWindows.withMask.Masks;
 import ar.edu.itba.ati.model.transformations.slidingWindows.withMask.SlidingWindowWithMask;
 
 public class GaussianSmoothing extends SlidingWindowWithMask<Double> {
     public GaussianSmoothing(double sigma) {
-        super(gaussianMask(calculateSize(sigma), sigma));
+        super(Masks.gaussianMask(calculateSize(sigma), sigma));
     }
 
     private static int calculateSize(double sigma) {
@@ -18,31 +19,5 @@ public class GaussianSmoothing extends SlidingWindowWithMask<Double> {
         }
 
         return size % 2 == 0 ? size - 1 : size;
-    }
-
-    //TODO pasar a masks
-    private static Mask<Double> gaussianMask(int size, double sigma) {
-        Double[][] matrix = new Double[size][size];
-        int halfSize = size / 2;
-        double accum = 0;
-        double value, x, y;
-
-        for(int row = 0; row < size; row++){
-            for(int col = 0; col < size; col++){
-                x = row - halfSize;
-                y = col  - halfSize;
-                value = (1 / (2 * Math.PI * sigma * sigma)) * Math.exp(- (x * x + y * y) / (sigma * sigma));
-                matrix[row][col] = value;
-                accum += value;
-            }
-        }
-
-        for(int row = 0; row < size; row++){
-            for(int col = 0; col < size; col++){
-                matrix[row][col] /= accum;
-            }
-        }
-
-        return new Mask(matrix);
     }
 }
